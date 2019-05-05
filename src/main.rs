9fef031955305b7e89ccc4b9abf2c417c14b9ddd
robotbrain <robotbrainify@gmail.com>
2019-05-05 13:23:27 -0400

    It uses environment variables because not being stupid

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,15 @@ use serenity::client::Client;
 use serenity::prelude::EventHandler;
 use serenity::framework::standard::StandardFramework;
 
+use std::env;
+
 struct Handler;
 
 impl EventHandler for Handler {}
 
 fn main() {
     // Login with a bot token from the environment
-    let mut client = Client::new(include_str!("../token"), Handler)
+    let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
         .expect("Error creating client");
     client.with_framework(StandardFramework::new()
         .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
